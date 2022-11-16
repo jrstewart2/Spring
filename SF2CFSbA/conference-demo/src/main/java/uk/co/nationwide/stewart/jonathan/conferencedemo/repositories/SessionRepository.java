0776694142692452204fb37b900ee643b3f5538c
_|¤n@ƫħαη$tΞ₩4rŦ <117824372+jrstewart2@users.noreply.github.com>
2022-11-16 09:52:54 +0000

    Delete SF2CFSbA/conference-demo directory

--- SF2CFSbA/conference-demo/src/main/java/uk/co/nationwide/stewart/jonathan/conferencedemo/repositories/SessionRepository.java
+++
@@ -1,7 +0,0 @@
-package uk.co.nationwide.stewart.jonathan.conferencedemo.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import uk.co.nationwide.stewart.jonathan.conferencedemo.models.Session;
-
-public interface SessionRepository extends JpaRepository<Session, Long> {
-}
--- SF2CFSbA/conference-demo/src/main/java/uk/co/nationwide/stewart/jonathan/conferencedemo/repositories/SpeakerRepository.java
+++
@@ -1,7 +0,0 @@
-package uk.co.nationwide.stewart.jonathan.conferencedemo.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import uk.co.nationwide.stewart.jonathan.conferencedemo.models.Speaker;
-
-public interface SpeakerRepository extends JpaRepository<Speaker, Long> {
-}
