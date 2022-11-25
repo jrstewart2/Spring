d03dda2ef34aafd72a9e9b344e7f793588989784
jrstewart2 <jonathan.stewart2@nationwide.co.uk>
2022-11-25 16:49:29 +0000

    facebook login working

--- spring-security-oauth/src/main/java/stewart/jonathan/springsecurityoauth/SpringSecurityOauthApplication.java
+++ spring-security-oauth/src/main/java/stewart/jonathan/springsecurityoauth/SpringSecurityOauthApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringSecurityOauthApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(SpringSecurityOauthApplication.class, args);
 	}
 
